7b3896e2d8371791dae5794cf9c4c4158d8ceb56
matuxer <matias@biott.com.ar>
2024-11-29 18:39:46 -0300

    Add User model with basic fields

---
+++ src/users/users.model.ts
@@ -0,0 +1,33 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'users',
+  timestamps: true,
+})
+export class User extends Model {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
+  })
+  id: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+}
