c7e5e1c78e213ae83576d92e2a2d2f1f4cc82a32
matuxer <matias@biott.com.ar>
2024-11-29 18:40:50 -0300

    Add UpdateUserDto for user updates

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  password?: string;
+}
