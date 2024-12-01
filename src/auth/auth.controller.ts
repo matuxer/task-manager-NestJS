a484780cbc98da01e2996dbb7e0fde05d4938f1e
matuxer <matias@biott.com.ar>
2024-12-01 17:07:29 -0300

    Add register endpoint and improve login response type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthResponse } from 'src/types/authentication-response.type';
+import { User } from 'src/users/users.model';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() loginUserDto: LoginUserDto): Promise<any> {
-    return this.authService.login(loginUserDto);
+  async login(@Body() loginUserDto: LoginUserDto): Promise<AuthResponse<User>> {
+    return await this.authService.login(loginUserDto);
+  }
+
+  @Post('register')
+  async register(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<AuthResponse<User>> {
+    try {
+      return await this.authService.register(createUserDto);
+    } catch (error) {
+      if (error.status === HttpStatus.BAD_REQUEST) {
+        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+      }
+
+      throw new HttpException(
+        'Error al registrar el usuario',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
