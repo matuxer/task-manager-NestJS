6b10c219f67688e5aa7ebbdad4e0adb3bef016aa
matuxer <matias@biott.com.ar>
2024-12-04 11:08:04 -0300

    Enable CORS for local development

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,12 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
