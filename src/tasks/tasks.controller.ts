2b2122660648d8050bf0f79a1cb2dc5af03a9ee6
matuxer <matias@biott.com.ar>
2024-12-04 11:03:07 -0300

    Add JWT authentication guard to task controller endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,22 +7,26 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { TaskService } from './tasks.service';
 import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { PaginationResponse } from 'src/types/pagination-response.type';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return await this.taskService.createTask(createTaskDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAllTasks(
     @Query('page') page: number = 1,
@@ -31,11 +35,13 @@ export class TaskController {
     return await this.taskService.getAllTasks(page, limit);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getTaskById(@Param('id') id: string): Promise<Task> {
     return await this.taskService.getTaskById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   async updateTask(
     @Param('id') id: string,
@@ -44,6 +50,7 @@ export class TaskController {
     return await this.taskService.updateTask(id, updateTaskDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async deleteTask(@Param('id') id: string): Promise<{ message: string }> {
     return await this.taskService.deleteTask(id);
